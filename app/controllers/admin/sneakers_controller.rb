c8e1da6129019afbf8d619d40ddcbb3c885172b2
wratterman <will.ratterman@gmail.com>
2017-07-19 11:39:24 -0600

    final branch commit

--- app/controllers/admin/sneakers_controller.rb
+++ app/controllers/admin/sneakers_controller.rb
@@ -1,5 +1,50 @@
 class Admin::SneakersController < Admin::MasterController
+  before_action :set_sneaker, only: [:show, :edit, :update, :destroy]
 
   def index
+    @sneakers = Sneaker.all
+  end
+
+  def new
+    @sneaker = Sneaker.new
+  end
+
+  def create
+    @sneaker = Sneaker.new(sneakers_params)
+    if @sneaker.save
+      #do a thing
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @sneaker.update(sneakers_params)
+      #do a thing
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @sneaker.destroy
+    #promt user + redirect
+  end
+
+  private
+
+  def set_sneaker
+    @sneaker = Sneaker.find(params[:id])
+  end
+
+  def sneaker_params
+    params.require(:sneaker).permit(:name, :description, :image_url,
+                                    :price, :footlocker_url)
   end
 end
