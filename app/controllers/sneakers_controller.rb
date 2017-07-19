4b65d3fa62e8cc0bc9d758505cb4af8978ae28df
wratterman <will.ratterman@gmail.com>
2017-07-19 11:43:07 -0600

    cleaned up sneakers_controller and removed non-Admin::SneakersController functionality

--- app/controllers/sneakers_controller.rb
+++ app/controllers/sneakers_controller.rb
@@ -1,56 +1,12 @@
 class SneakersController < ApplicationController
 
-  before_action :set_sneaker, only: [:show, :edit, :update, :destroy]
-
-  def new
-    @sneaker = Sneaker.new
-  end
-
-  def create
-    @sneaker = Sneaker.new(sneakers_params)
-    if @sneaker.save
-      flash[:success] = "#{@sneaker.name} created!"
-      redirect_to @sneaker
-    else
-      render :new
-    end
-  end
+  before_action :set_sneaker, only: [:show]
 
   def index
     @sneakers = Sneaker.all
   end
 
   def show
-  end
-
-  def edit
-  end
-
-  def update
-    if @sneaker.update(sneakers_params)
-      flash[:success] = "#{@sneaker.name} updated!"
-      redirect_to @sneaker
-    else
-      render :edit
-    end
-  end
-
-  def destroy
-    sneaker = @sneaker
-    sneaker.destroy
-    flash[:delete] = "#{sneaker.name} deleted!"
-
-    redirect_to sneakers_path
-  end
-
-  private
-
-  def sneakers_params
-    params.require[:sneaker].permit[:name, :description, :image_url,
-                                    :price, :footlocker_url]
-  end
-
-  def set_sneaker
     @sneaker = Sneaker.find(params[:id])
   end
 end
